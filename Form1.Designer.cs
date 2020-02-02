d409064733b298675b1cd1396cd8dfdb45502879
Daoi <36860391+Daoi@users.noreply.github.com>
2020-02-01 19:01:56 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace TaxCalculator
+{
+    partial class frmTaxCalc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtTaxableIncome = new System.Windows.Forms.TextBox();
+            this.txtTaxOwed = new System.Windows.Forms.TextBox();
+            this.lblTaxableIncome = new System.Windows.Forms.Label();
+            this.lblTaxOwed = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Enabled = false;
+            this.btnCalculate.Location = new System.Drawing.Point(112, 156);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(234, 156);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtTaxableIncome
+            // 
+            this.txtTaxableIncome.Enabled = false;
+            this.txtTaxableIncome.Location = new System.Drawing.Point(174, 64);
+            this.txtTaxableIncome.Name = "txtTaxableIncome";
+            this.txtTaxableIncome.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxableIncome.TabIndex = 2;
+            // 
+            // txtTaxOwed
+            // 
+            this.txtTaxOwed.Enabled = false;
+            this.txtTaxOwed.Location = new System.Drawing.Point(174, 100);
+            this.txtTaxOwed.Name = "txtTaxOwed";
+            this.txtTaxOwed.ReadOnly = true;
+            this.txtTaxOwed.Size = new System.Drawing.Size(100, 20);
+            this.txtTaxOwed.TabIndex = 3;
+            // 
+            // lblTaxableIncome
+            // 
+            this.lblTaxableIncome.AutoSize = true;
+            this.lblTaxableIncome.Enabled = false;
+            this.lblTaxableIncome.Location = new System.Drawing.Point(77, 64);
+            this.lblTaxableIncome.Name = "lblTaxableIncome";
+            this.lblTaxableIncome.Size = new System.Drawing.Size(86, 13);
+            this.lblTaxableIncome.TabIndex = 4;
+            this.lblTaxableIncome.Text = "Taxable Income:";
+            // 
+            // lblTaxOwed
+            // 
+            this.lblTaxOwed.AutoSize = true;
+            this.lblTaxOwed.Enabled = false;
+            this.lblTaxOwed.Location = new System.Drawing.Point(77, 103);
+            this.lblTaxOwed.Name = "lblTaxOwed";
+            this.lblTaxOwed.Size = new System.Drawing.Size(91, 13);
+            this.lblTaxOwed.TabIndex = 5;
+            this.lblTaxOwed.Text = "Income tax owed:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(173, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 6;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(279, 10);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 7;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(80, 19);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(66, 13);
+            this.lblName.TabIndex = 8;
+            this.lblName.Text = "Enter Name:";
+            // 
+            // frmTaxCalc
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(413, 205);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblTaxOwed);
+            this.Controls.Add(this.lblTaxableIncome);
+            this.Controls.Add(this.txtTaxOwed);
+            this.Controls.Add(this.txtTaxableIncome);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "frmTaxCalc";
+            this.Text = "Income Tax Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtTaxableIncome;
+        private System.Windows.Forms.TextBox txtTaxOwed;
+        private System.Windows.Forms.Label lblTaxableIncome;
+        private System.Windows.Forms.Label lblTaxOwed;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace TaxCalculator
+{
+    //Look up tax bracket and calculate
+    //Taxable income
+    public partial class frmTaxCalc : Form
+    {
+        //Tax Table
+        //The key for the table is a lambda function that compares the value put in
+        //To the ranges for the tax brackets
+        //It then returns a string which is split, to get the values for the flat amount,
+        //The percent extra, and the "over excess" amount (i.e. the previous taxbracket's top end)
+         Dictionary<Func<decimal, bool>, string> taxTable = new Dictionary<Func<decimal, bool>, string>()
+            {
+                {taxableIncome => taxableIncome >= 0.0m && taxableIncome <= 9225.0m, "0|.10|0"  },
+                {taxableIncome => taxableIncome >= 9225.01m && taxableIncome <= 37450.0m, "922.50|.15|9225"  },
+                {taxableIncome => taxableIncome >= 37450.01m && taxableIncome <= 90750.0m, "5156.25|.25|37450"  },
+                {taxableIncome => taxableIncome >= 90750.01m && taxableIncome <= 189300.0m, "18481.25|.28|90750"  },
+                {taxableIncome => taxableIncome >= 189300.01m && taxableIncome <= 411500.0m, "46075.25|.33|189300"  },
+                {taxableIncome => taxableIncome >= 411500.01m && taxableIncome <= 413200.0m, "119401.25|.35|411500"  },
+                {taxableIncome => taxableIncome >= 413200.01m, "119996.25|.396|413200"  },
+            };
+
+
+
+        public frmTaxCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            if (!Regex.IsMatch(txtName.Text, @"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$")) //Regex to check for valid name
+            {
+                MessageBox.Show("Please enter a valid name to continue(Alphabet only)", "Error!",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtName.Focus();
+            }
+            else {
+                lblTaxableIncome.Enabled = true;
+                txtTaxableIncome.Enabled = true;
+                lblTaxOwed.Enabled = true;
+                txtTaxOwed.Enabled = true;
+                btnCalculate.Enabled = true;
+                txtTaxableIncome.Focus();
+            }
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (validateIncome(txtTaxableIncome.Text)) { 
+            txtTaxOwed.Text = (taxTableLookup(Convert.ToDecimal(txtTaxableIncome.Text))).ToString("c");
+            }
+            else
+            {
+                
+            }
+        }
+
+        //Creates a lambda function to pass into the dictionry as a key
+        //Passes the argument into the lambda function for the dictionary key
+        //Gets the string value, parses for appropriate values for calculation
+        //returns taxable income
+        private decimal taxTableLookup(decimal taxableIncome)
+        {
+            var key = taxTable.Keys.Single(tI => tI(taxableIncome));
+            string[] value = taxTable[key].Split('|');
+
+            return Convert.ToDecimal(value[0]) + (Convert.ToDecimal(value[1]) * (taxableIncome - Convert.ToDecimal(value[2])));       
+
+        }
+        //Validates the income using try/catch/check for negative value
+        private bool validateIncome(string value)
+        {
+            try
+            {
+                decimal income = Convert.ToDecimal(value);
+                if(income <= 0)
+                {
+                    MessageBox.Show("Invalid invoice value, should be > 0", "Error!",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    txtTaxableIncome.Text = "";
+                    txtTaxableIncome.Focus();
+                    return false;
+
+                }
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Invalid income value, should be numeric only and greater than 0", "Error!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtTaxableIncome.Text = "";
+                txtTaxableIncome.Focus();
+                return false;
+            }
+
+        }
+    }
+}
